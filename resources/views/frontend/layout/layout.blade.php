794646812a010340ab8c37439e1034f341676ebd
Sandhya Gauro <sandhya@view9.com.np>
2018-12-11 11:35:19 +0545

    messenger integration

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -66,21 +66,6 @@
     <script>
 
 //       messenger integration
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId            : '196086948005019',
-                autoLogAppEvents : true,
-                xfbml            : true,
-                version          : 'v2.11'
-            });
-        };
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
 
 
         $(document).ready(function() {
@@ -138,6 +123,22 @@
                 }
             });
         })
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId            : '196086948005019',
+                autoLogAppEvents : true,
+                xfbml            : true,
+                version          : 'v2.11'
+            });
+        };
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
     </script>
 
 </div>
