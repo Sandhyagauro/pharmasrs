9c67689577ef2deb8e49f2624c7dafd2a158568a
Deepak shrestha <deepak@view9.com.np>
2018-12-25 11:41:38 +0545

    email template

--- resources/views/frontend/email/contact.blade.php
+++ resources/views/frontend/email/contact.blade.php
@@ -1,59 +1,104 @@
+<body style="background: #0484c2">
+<div style="border-bottom: 1px solid #eee; padding: 30px 35px 35px 35px; margin: auto;display: block;width:500px;
+    box-shadow: 0 0 22px #848181;background: #fff;margin-top:40px">
+<img src="img/logo.png" style="display: block;margin: auto;width: 120px;height:100px;object-fit: contain;">
+<h3 style="color: #848484;
+font-family: 'Open Sans', sans-serif;font-size: 16px;font-weight: 400;text-align: center;">
+You got a message from Contact Form
+</h3>
+<hr>
 
-<div style="border-bottom: 1px solid #eee; padding: 30px 35px 35px 35px;">
-    <h3 style="color: #000000; font-family: 'Open Sans', sans-serif; font-size: 28px; font-weight: 300; margin: 0; line-height: 46px; text-align: left;">
-        You got a message from <br>Contact Form<br>
-    </h3>
-    <p style="color: #737373; font-family: 'Open Sans', sans-serif; font-size: 16px; line-height: 28px; font-weight: 400; text-align: left; margin: 40px 0 0 0;">
+<div><a href="pharma.com.np" style="text-decoration: none"><button style="background: #0879ba;
+    padding: 12px 35px;
+    color: #fff;
+    margin: auto;
+    display: block;
+    box-shadow: 0 0 6px #c3c3c3;
+    border: 0;
+    border-radius: 20px;
+    margin-top: 20px;
+    font-size: 15px;">Visit Website</button></a></div>
+<p style="color: #737373; font-family: 'Open Sans', sans-serif; font-size: 16px; line-height: 28px; font-weight: 400; text-align: left; margin: 40px 0 0 0;">
     <table id="customers" style=" font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
     border-collapse: collapse;
     width: 100%;">
     <tr>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px; color:black; ">Name:</th>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px; color:black; "> {{$name}}</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400; ">Name:</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400; "> {{$name}}</th>
 
     </tr>
 
     <tr>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px; color:black; ">Address:</th>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px; color:black;  "> {{$address}}</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400; ">Address:</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400;  "> {{$address}}</th>
 
     </tr>
     <tr>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px; color:black; ">Mobile:</th>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px; color:black;  "> {{$mobile}}</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400; ">Mobile:</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400;  "> {{$mobile}}</th>
 
     </tr>
 
     <tr>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px;  color:black;">Email:</th>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px;  color:black; "> {{$email}}</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400;">Email:</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400;"> {{$email}}</th>
 
     </tr>
     <tr>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px; color:black; ">Message:</th>
-        <th style=" border: 1px solid #ddd;
-    padding: 8px;  color:black; "> {{$content}}</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400; ">Message:</th>
+        <th style="
+        padding: 8px;
+        color: #757575;
+        font-size: 12px;
+        font-weight: 400;"> {{$content}}</th>
 
     </tr>
-    </table>
+</table>
 
+</p>
+<hr>
+<ul style="color: #464646; margin-left: 0px;font-family: 'Open Sans', sans-serif; font-size: 16px; line-height: 28px; font-weight: 400; text-align: center; margin: 35px 0 0 0; padding: 0; list-style: none;">
+    <li>
+    Pharma SRS        </li>
+    <li style="text-align: center;font-size: 15px">Copyrights © 2019. All Rights Reserved by Pharma SRS</li>
 
-
-
-
-    </p>
-    <ul style="color: #464646; margin-left: 0px;font-family: 'Open Sans', sans-serif; font-size: 16px; line-height: 28px; font-weight: 400; text-align: left; margin: 35px 0 0 0; padding: 0; list-style: none;">
-        <li>
-Pharma SRS        </li>
-
-    </ul>
+</ul>
 </div>
+
+</body>
